0db9fa11a32af8f9886be0b150fe2b1635699eea
YashKr01 <yashkumar201301@gmail.com>
2022-02-11 17:45:56 +0530

    Utils & Constants

---
+++ app/src/main/java/com/example/sample/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.sample.utils
+
+object Constants {
+
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val API_KEY = "04b7811d86a441118ca539537def9b62"
+
+    const val WEATHER_BASE_URL = "http://api.weatherapi.com/v1/"
+    const val WEATHER_API_KEY = "e15a278246414a5abea182114211811"
+
+}
