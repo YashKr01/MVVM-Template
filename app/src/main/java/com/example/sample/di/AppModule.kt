39dac07100bd0f472f7c05e09ef48d343c6d7fa7
YashKr01 <yashkumar201301@gmail.com>
2022-02-13 17:59:51 +0530

    DI setup

--- app/src/main/java/com/example/sample/di/AppModule.kt
+++ app/src/main/java/com/example/sample/di/AppModule.kt
@@ -5,16 +5,48 @@ import androidx.room.Room
 import com.example.sample.database.TaskDao
 import com.example.sample.utils.Constants.DATABASE_NAME
 import com.example.sample.database.TaskDatabase
+import com.example.sample.network.ApiInterface
+import com.example.sample.utils.Constants.WEATHER_BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    @Provides
+    @Singleton
+    fun provideClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(WEATHER_BASE_URL)
+            .client(okHttpClient)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiInterface =
+        retrofit.create(ApiInterface::class.java)
 
     @Provides
     @Singleton
--- app/src/main/java/com/example/sample/network/ApiInterface.kt
+++ app/src/main/java/com/example/sample/network/ApiInterface.kt
@@ -2,15 +2,15 @@ package com.example.sample.network
 
 import com.example.sample.model.NewsResponse
 import com.example.sample.model.WeatherResponse
-import com.example.sample.utils.Constants.API_KEY
 import com.example.sample.utils.Constants.WEATHER_API_KEY
 import retrofit2.http.GET
 import retrofit2.http.Query
+import retrofit2.http.Url
 
 interface ApiInterface {
 
-    @GET("top-headlines?country=in&apiKey=$API_KEY")
-    suspend fun getTopHeadlines(): NewsResponse
+    @GET
+    suspend fun getTopHeadlines(@Url newsUrl: String): NewsResponse
 
     @GET("forecast.json?key=$WEATHER_API_KEY")
     suspend fun getWeatherResponse(@Query("q") name: String) : WeatherResponse
